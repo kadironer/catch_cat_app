ab3492dd68fc9cbf646ec3fd40ba151bf6f6d3a8
Kadir <kadironer46500@gmail.com>
2022-12-02 18:33:25 +0300

    finish_application

--- catch_cat_app/ViewController.swift
+++ catch_cat_app/ViewController.swift
@@ -26,12 +26,26 @@ class ViewController: UIViewController {
     
     
     var timer = Timer()
+    var hideTimer = Timer()
     var score = 0
     var counter = 0
     var highScore = 0
+    var kediArray = [UIImageView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let storedHighScore = UserDefaults.standard.object(forKey: "highScoreKey")
+        
+        if storedHighScore == nil {
+            highScore = 0
+            highScore_label.text = "En Yüksek Skor: \(highScore)"
+        }
+        if let newScore = storedHighScore as? Int {
+            highScore = newScore
+            highScore_label.text = "En Yüksek Skor: \(highScore)"
+        }
+        
         
         kedi1.isUserInteractionEnabled = true
         kedi2.isUserInteractionEnabled = true
@@ -63,13 +77,28 @@ class ViewController: UIViewController {
         kedi8.addGestureRecognizer(gestureCats8)
         kedi9.addGestureRecognizer(gestureCats9)
         
-        counter = 10
+        kediArray = [kedi1, kedi2, kedi3, kedi4, kedi5, kedi6, kedi7, kedi8, kedi9]
+        
+        
+        counter = 15
         score = 0
         score_label.text = "Skor: \(score)"
         time_label.text = "\(counter)"
         highScore_label.text = "En Yüksek Skor: \(highScore)"
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(cathCatsFunc), userInfo: nil, repeats: true)
+        hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(hideCats), userInfo: nil, repeats: true)
+        
+    }
+    @objc func hideCats() {
+        
+        for kediler in kediArray {
+            kediler.isHidden = true
+            
+        }
+        let random = Int(arc4random_uniform(UInt32(kediArray.count-1)))
+        kediArray[random].isHidden = false
+        
     }
     
     @objc func cathCatsFunc(){
@@ -77,9 +106,35 @@ class ViewController: UIViewController {
         time_label.text = "\(counter)"
         if counter == 0 {
             timer.invalidate()
+            hideTimer.invalidate()
+            time_label.text = "Süre Bitti"
+            
+            
+            if self.score > self.highScore {
+                self.highScore = self.score
+                highScore_label.text = "En Yüksek Skor: \(self.highScore)"
+                UserDefaults.standard.set(self.highScore, forKey: "highScoreKey")
+            }
+            
             let alert = UIAlertController(title: "Süre Bitti", message: "Süreniz Bitti Skorunuz: \(score)", preferredStyle: UIAlertController.Style.alert)
-            let restartButton = UIAlertAction(title: "Yeniden Başla", style: UIAlertAction.Style.default)
+            let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.cancel)
+            let restartButton = UIAlertAction(title: "Yeniden Başla", style: UIAlertAction.Style.default) { UIAlertAction in
+                
+                self.score = 0
+                self.score_label.text = "Skor: \(self.score)"
+                self.counter = 15
+                self.time_label.text = "\(self.counter)"
+                self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.cathCatsFunc), userInfo: nil, repeats: true)
+                self.hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.hideCats), userInfo: nil, repeats: true)
+                
+                
+            }
+            for kediler in kediArray {
+                kediler.isHidden = true
+                
+            }
             alert.addAction(restartButton)
+            alert.addAction(okButton)
             self.present(alert, animated: true)
         }
     }
