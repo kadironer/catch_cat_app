c8a2a2155db930be383bb5ac61556351a84535de
Kadir <kadironer46500@gmail.com>
2022-12-02 17:37:47 +0300

    skor arttırma tamam

--- catch_cat_app/ViewController.swift
+++ catch_cat_app/ViewController.swift
@@ -13,14 +13,58 @@ class ViewController: UIViewController {
     @IBOutlet weak var time_label: UILabel!
     @IBOutlet weak var highScore_label: UILabel!
     
+    
+    @IBOutlet weak var kedi1: UIImageView!
+    @IBOutlet weak var kedi2: UIImageView!
+    @IBOutlet weak var kedi3: UIImageView!
+    @IBOutlet weak var kedi4: UIImageView!
+    @IBOutlet weak var kedi5: UIImageView!
+    @IBOutlet weak var kedi6: UIImageView!
+    @IBOutlet weak var kedi7: UIImageView!
+    @IBOutlet weak var kedi8: UIImageView!
+    @IBOutlet weak var kedi9: UIImageView!
+    
+    
     var timer = Timer()
     var score = 0
     var counter = 0
     var highScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        counter = 20
+        kedi1.isUserInteractionEnabled = true
+        kedi2.isUserInteractionEnabled = true
+        kedi3.isUserInteractionEnabled = true
+        kedi4.isUserInteractionEnabled = true
+        kedi5.isUserInteractionEnabled = true
+        kedi6.isUserInteractionEnabled = true
+        kedi7.isUserInteractionEnabled = true
+        kedi8.isUserInteractionEnabled = true
+        kedi9.isUserInteractionEnabled = true
+        
+        let gestureCats1 = UITapGestureRecognizer(target: self, action: #selector(scoreArttır))
+        let gestureCats2 = UITapGestureRecognizer(target: self, action: #selector(scoreArttır))
+        let gestureCats3 = UITapGestureRecognizer(target: self, action: #selector(scoreArttır))
+        let gestureCats4 = UITapGestureRecognizer(target: self, action: #selector(scoreArttır))
+        let gestureCats5 = UITapGestureRecognizer(target: self, action: #selector(scoreArttır))
+        let gestureCats6 = UITapGestureRecognizer(target: self, action: #selector(scoreArttır))
+        let gestureCats7 = UITapGestureRecognizer(target: self, action: #selector(scoreArttır))
+        let gestureCats8 = UITapGestureRecognizer(target: self, action: #selector(scoreArttır))
+        let gestureCats9 = UITapGestureRecognizer(target: self, action: #selector(scoreArttır))
+        
+        kedi1.addGestureRecognizer(gestureCats1)
+        kedi2.addGestureRecognizer(gestureCats2)
+        kedi3.addGestureRecognizer(gestureCats3)
+        kedi4.addGestureRecognizer(gestureCats4)
+        kedi5.addGestureRecognizer(gestureCats5)
+        kedi6.addGestureRecognizer(gestureCats6)
+        kedi7.addGestureRecognizer(gestureCats7)
+        kedi8.addGestureRecognizer(gestureCats8)
+        kedi9.addGestureRecognizer(gestureCats9)
+        
+        counter = 10
+        score = 0
         score_label.text = "Skor: \(score)"
         time_label.text = "\(counter)"
         highScore_label.text = "En Yüksek Skor: \(highScore)"
@@ -38,6 +82,11 @@ class ViewController: UIViewController {
             alert.addAction(restartButton)
             self.present(alert, animated: true)
         }
+    }
+    
+    @objc func scoreArttır(){
+        score = score + 1
+        score_label.text = "Skor: \(score)"
     }
 
 
